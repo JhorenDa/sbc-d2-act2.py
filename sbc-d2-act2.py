bfbd8c4c9891a48fadaa80d1e4e4c3f4c8441f00
JhorenDa <jhoren.pana@aclcbutuan.edu.ph>
2024-06-19 20:59:47 +0800

    first commit

---
+++ sbc-d2-act2.py
@@ -0,0 +1,77 @@
+#-------------------------------------------------------------------------------------------------------------------------
+#PEMDAS
+'''
+print((1+1)**1*1/1+1-1)
+print(1+1/1*(1+1)**1-1)
+'''
+#-------------------------------------------------------------------------------------------------------------------------
+#random numbers
+'''
+import random
+print(random.randint(0,9))
+'''
+'''
+from random import randint
+num = randint(0,9)
+print(num)
+'''
+#-------------------------------------------------------------------------------------------------------------------------
+#if statement
+'''
+age = 12
+if age < 18:
+    print("Mapriso ka!")
+if age < 60:
+    print("Ayaw na tay")
+else: 
+    print("Pwede na")
+'''
+
+from random import randint
+
+choices0 = "fold"
+choices1 = "unfold"
+ran = randint(0,1)
+dan = randint(0,1)
+
+user = input("Choose Fold or Unfold >")
+if user.capitalize() == "Fold":
+    p1 = "Fold"
+    print("Player 1 = ", user)
+else:
+    p1 = "Unfold"
+    print("Player 1 = Unfold")
+
+com1 = ran
+if com1 == 1:
+    com1 = "Fold"
+    print("Computer 1 = Fold")
+else:
+    com1 = "Unfold"
+    print("Computer 1 = Unfold")
+
+com2 = dan
+if com2 == 1:
+    com2 = "Fold"
+    print("Computer 2 = Fold")
+else:
+    com2 = "Unfold"
+    print("Computer 2 = Unfold")
+
+
+if (p1 == "Fold" and com1 == "Unfold" and com2 == "Unfold") or (p1 == "Unfold" and com1 == "Fold" and com2 == "Fold"):
+    print("Player 1 Win!!!!")
+
+
+elif (p1 == "Unfold" and com1 == "Fold" and com2 == "Unfold") or (p1 == "Fold" and com1 == "Unfold" and com2 == "Fold"):
+    print("Computer 1 Win!!!!")
+
+
+elif (p1 == "Unfold" and com1 == "Unfold" and com2 == "Fold") or (p1 == "Fold" and com1 == "Fold" and com2 == "Unfold"):
+    print("Computer 2 Win!!!!")
+
+
+else:
+    print("No one wins!!!")
+
+
